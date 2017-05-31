b396960cd5d3d7f231dcc7849a549e7a7592cd34
rrayleigh <singhsatish@outlook.in>
2017-05-31 14:30:09 +0530

    Add files via upload

---
+++ Shapes.java
@@ -0,0 +1,35 @@
+package javaassignments;
+
+class Rectangle {
+	int l, b;
+
+	Rectangle(int x, int y) {
+		l = x;
+		b = y;
+	}
+
+	int getRectangle() {
+		return l * b;
+	}
+}
+
+class Triangle extends Rectangle {
+	float a;
+
+	Triangle(int v, int u) {
+		super(u, v);
+	}
+
+	float getTriangle() {
+		a = (float) l / 2 * l * b;
+		return (a);
+	}
+}
+
+class RectangleTriangle {
+	public static void main(String args[]) {
+		Triangle m = new Triangle(500, 80);
+		System.out.println("Area of Rectangle is : " + m.getRectangle());
+		System.out.println("Area of Triangle is : " + m.getTriangle());
+	}
+}
